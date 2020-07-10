f2be36022aab2f58e41aa4270d6792252d7e5bb0
Matt Koster <mkoster@stack41.com>
2020-07-09 22:45:21 -0500

    Comment

--- web/app.py
+++ web/app.py
@@ -24,3 +24,17 @@ def index():
 
 if __name__ == '__main__':
     app.run()
+
+
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     if request.method == 'POST':
+#         text = request.form['text']
+#         post = Post(text)
+#         db.session.add(post)
+#         db.session.commit()
+#     posts = Post.query.order_by(Post.date_posted.desc()).all()
+#     return render_template('index.html', posts=posts)
+
+# if __name__ == '__main__':
+#     app.run()
